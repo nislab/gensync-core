829b68d3ec4391d516cf0aed3d0ce89b33cca592
Anish701 <anish2sinha@gmail.com>
2023-11-13 23:18:42 -0500

    implemented client side communication for BloomFilterSync

--- src/Syncs/BloomFilterSync.cpp
+++ src/Syncs/BloomFilterSync.cpp
@@ -13,8 +13,44 @@ BloomFilterSync::~BloomFilterSync() = default;
 
 bool BloomFilterSync::SyncClient(const shared_ptr<Communicant>& commSync, list<shared_ptr<DataObject>> &selfMinusOther, list<shared_ptr<DataObject>> &otherMinusSelf)
 {
+	Logger::gLog(Logger::METHOD, "Entering BloomFilterSync::SyncClient");
 
-	return false;
+        bool success = true;
+
+        // call parent method for bookkeeping
+        SyncMethod::SyncClient(commSync, selfMinusOther, otherMinusSelf);
+
+        // connect to server
+        mySyncStats.timerStart(SyncStats::IDLE_TIME);
+        commSync->commConnect();
+        mySyncStats.timerEnd(SyncStats::IDLE_TIME);
+
+	mySyncStats.timerStart(SyncStats::COMM_TIME);
+	commSync->commSend(myBloomFilter.toString());
+        mySyncStats.timerEnd(SyncStats::COMM_TIME);
+
+	// Computation
+	mySyncStats.timerStart(SyncStats::COMM_TIME);
+        list<shared_ptr<DataObject>> newOMS = commSync->commRecv_DataObject_List();
+        list<shared_ptr<DataObject>> newSMO = commSync->commRecv_DataObject_List();
+        mySyncStats.timerEnd(SyncStats::COMM_TIME);
+
+        mySyncStats.timerStart(SyncStats::COMP_TIME);
+        otherMinusSelf.insert(otherMinusSelf.end(), newOMS.begin(), newOMS.end());
+        selfMinusOther.insert(selfMinusOther.end(), newSMO.begin(), newSMO.end());
+        mySyncStats.timerEnd(SyncStats::COMP_TIME);
+
+        stringstream msg;
+        msg << "BloomFilterSync succeeded." << endl;
+        msg << "self - other = " << printListOfSharedPtrs(selfMinusOther) << endl;
+        msg << "other - self = " << printListOfSharedPtrs(otherMinusSelf) << endl;
+        Logger::gLog(Logger::METHOD, msg.str());
+
+	// Record Stats
+	mySyncStats.increment(SyncStats::XMIT,commSync->getXmitBytes());
+        mySyncStats.increment(SyncStats::RECV,commSync->getRecvBytes());
+
+        return success;
 }
 
 bool BloomFilterSync::SyncServer(const shared_ptr<Communicant>& commSync, list<shared_ptr<DataObject>> &selfMinusOther, list<shared_ptr<DataObject>> &otherMinusSelf)
