6bcddbe1a6ef53cb04c96f9f2c0869ebb63db18d
Anish701 <anish2sinha@gmail.com>
2023-12-04 00:43:35 -0500

    commit for BloomFilter unit tests

---
+++ tests/unit/BloomFilterTest.cpp
@@ -0,0 +1,99 @@
+#include <climits>
+#include "BloomFilterTest.h"
+
+CPPUNIT_TEST_SUITE_REGISTRATION(BloomFilterTest);
+
+BloomFilterTest::BloomFilterTest() {
+}
+
+BloomFilterTest::~BloomFilterTest() {
+}
+
+void BloomFilterTest::setUp() {
+    const int SEED = 617;
+    srand(SEED);
+}
+
+void BloomFilterTest::tearDown() {}
+
+void BloomFilterTest::testBFBuild(){
+    int size = rand()%10;
+    int nHash = 2 + rand()%5;
+    
+    BloomFilter bf(size, nHash);
+
+    CPPUNIT_ASSERT(size == bf.getSize());
+    CPPUNIT_ASSERT(nHash == bf.getNumHashes());
+}
+
+void BloomFilterTest::testBFInsert(){
+    vector<ZZ> items;
+    const int SIZE = 50;
+    const size_t ITEM_SIZE = sizeof(randZZ());
+
+    for(int ii = 0; ii < SIZE; ii++)
+        items.push_back(randZZ());
+
+    BloomFilter bf(SIZE*4, 3);
+    int inserted = 0;
+
+    for(auto val: items)
+    {
+        // skip if hash collision
+        if(bf.exist(val))
+            continue;
+
+        string prev = bf.toString();
+        bf.insert(val);
+        CPPUNIT_ASSERT(prev != bf.toString());
+        inserted++;
+    }
+
+    // probabilistically highly likely be true
+    CPPUNIT_ASSERT(inserted > SIZE/2);
+}
+
+void BloomFilterTest::testBFExist(){
+    vector<ZZ> items;
+    const int SIZE = 50;
+    const size_t ITEM_SIZE = sizeof(randZZ());
+
+    for(int ii = 0; ii < SIZE; ii++)
+        items.push_back(randZZ());
+
+    BloomFilter bf(SIZE*2, 3);
+
+    for(auto val: items)
+    {
+        string prev = bf.toString();
+        bool prevExist = bf.exist(val);
+        bf.insert(val);
+
+        bool existFtoT = prevExist == false && bf.exist(val) == true;
+        bool hashCollision = prev == bf.toString() && prevExist == true;
+        CPPUNIT_ASSERT(existFtoT || hashCollision);
+    }
+}
+
+void BloomFilterTest::testBFBitsZZConv(){
+    vector<ZZ> items;
+    const int SIZE = 10;
+    const size_t ITEM_SIZE = sizeof(randZZ());
+
+    for(int ii = 0; ii < SIZE; ii++)
+        items.push_back(randZZ());
+
+    BloomFilter bf(SIZE*2, 3);
+
+    for(auto val: items)
+    {
+        bf.insert(val);
+
+        ulong longVal = stoul(bf.toString(), nullptr, 2);
+        ZZ ZZVal = bf.toZZ(bf.toString());
+        CPPUNIT_ASSERT(longVal == ZZVal);
+
+        string newBitString = bf.ZZtoBitString(ZZVal);
+        CPPUNIT_ASSERT(newBitString == bf.toString());
+    }
+}
---
+++ tests/unit/BloomFilterTest.h
@@ -0,0 +1,47 @@
+#ifndef CPISYNCLIB_BloomFilterTest_H
+#define CPISYNCLIB_BloomFilterTest_H
+
+#include <cppunit/extensions/HelperMacros.h>
+#include <GenSync/Syncs/BloomFilter.h>
+#include <GenSync/Aux/Auxiliary.h>
+#include <iostream>
+#include <algorithm>
+
+class BloomFilterTest : public CPPUNIT_NS::TestFixture{
+    CPPUNIT_TEST_SUITE(BloomFilterTest);
+    CPPUNIT_TEST(testBFBuild);
+    CPPUNIT_TEST(testBFInsert);
+    CPPUNIT_TEST(testBFExist);
+    CPPUNIT_TEST(testBFBitsZZConv);
+
+    CPPUNIT_TEST_SUITE_END();
+public:
+    BloomFilterTest();
+    virtual ~BloomFilterTest();
+    void setUp();
+    void tearDown();
+
+    /**
+     * Test constructor, setters, and getters of BloomFilter
+     */
+    static void testBFBuild();
+
+    /**
+     * Test insert into BloomFilter
+     */
+    static void testBFInsert();
+
+    /**
+     * Test BloomFilter exist method
+     */
+    static void testBFExist();
+
+    /**
+     * Test BloomFilter conversions between bit string and ZZ
+     */
+    static void testBFBitsZZConv();
+
+
+};
+
+#endif //CPISYNCLIB_BloomFilterTest_H
