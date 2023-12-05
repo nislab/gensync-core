281bb0d14344e52f15a3ac179cec0b25f54577d2
Anish Sinha <108232600+Anish701@users.noreply.github.com>
2023-12-05 10:36:59 -0500

    Fixed formatting of BloomFilterSyncTest.cpp

--- tests/unit/BloomFilterSyncTest.cpp
+++ tests/unit/BloomFilterSyncTest.cpp
@@ -14,106 +14,106 @@ BloomFilterSyncTest::BloomFilterSyncTest() = default;
 BloomFilterSyncTest::~BloomFilterSyncTest() = default;
 
 void BloomFilterSyncTest::setUp() {
-    const int SEED = 93;
-    srand(SEED);
+	const int SEED = 93;
+	srand(SEED);
 }
 
 void BloomFilterSyncTest::tearDown() {
 }
 
 void BloomFilterSyncTest::BloomFilterSyncSetReconcileTest() {
-    const int BITS = sizeof(randZZ());
-
+	const int BITS = sizeof(randZZ());
+	
 	GenSync GenSyncServer = GenSync::Builder().
 			setProtocol(GenSync::SyncProtocol::BloomFilterSync).
 			setComm(GenSync::SyncComm::socket).
 			setBits(BITS).
 			setExpNumElems(numExpElem).
 			build();
-
+	
 	GenSync GenSyncClient = GenSync::Builder().
 			setProtocol(GenSync::SyncProtocol::BloomFilterSync).
 			setComm(GenSync::SyncComm::socket).
 			setBits(BITS).
 			setExpNumElems(numExpElem).
 			build();
-
+	
 	//(oneWay = false, probSync = true, syncParamTest = false, Multiset = false, largeSync = false)
 	CPPUNIT_ASSERT(syncTest(GenSyncClient, GenSyncServer, false, true, false, false, false));
 }
 
 void BloomFilterSyncTest::BloomFilterSyncLargeSetReconcileTest(){
-    const int BITS = sizeof(randZZ());
-
+	const int BITS = sizeof(randZZ());
+	
 	GenSync GenSyncServer = GenSync::Builder().
 			setProtocol(GenSync::SyncProtocol::BloomFilterSync).
 			setComm(GenSync::SyncComm::socket).
 			setBits(BITS).
 			setExpNumElems(largeNumExpElems).
 			build();
-
+	
 	GenSync GenSyncClient = GenSync::Builder().
 			setProtocol(GenSync::SyncProtocol::BloomFilterSync).
 			setComm(GenSync::SyncComm::socket).
 			setBits(BITS).
 			setExpNumElems(largeNumExpElems).
 			build();
-
+	
 	//(oneWay = false, probSync = true, syncParamTest = false, Multiset = false, largeSync = true)
 	CPPUNIT_ASSERT(syncTest(GenSyncClient, GenSyncServer, false, true,false,false,true));
 }
 
 void BloomFilterSyncTest::testAddElem(){
-    // number of elems to add
-    const int ITEMS = 50;
-    GenSync bfSync = GenSync::Builder().
+	// number of elems to add
+	const int ITEMS = 50;
+	GenSync bfSync = GenSync::Builder().
 			setProtocol(GenSync::SyncProtocol::BloomFilterSync).
 			setComm(GenSync::SyncComm::socket).
 			setBits(4).
 			setExpNumElems(numExpElem).
 			build();
-    multiset<shared_ptr<DataObject>, cmp<shared_ptr<DataObject>>> elts;
-
-    // add items works
-    for(int ii = 0; ii < ITEMS; ii++) {
-        shared_ptr<DataObject> item = make_shared<DataObject>(randZZ());
-        elts.insert(item);
-        bfSync.addElem(item);
-    }
-
-    // check that all items added
-    CPPUNIT_ASSERT(bfSync.dumpElements().size() == ITEMS);
+	multiset<shared_ptr<DataObject>, cmp<shared_ptr<DataObject>>> elts;
+	
+	// add items works
+	for(int ii = 0; ii < ITEMS; ii++) {
+		shared_ptr<DataObject> item = make_shared<DataObject>(randZZ());
+		elts.insert(item);
+		bfSync.addElem(item);
+	}
+	
+	// check that all items added
+	CPPUNIT_ASSERT(bfSync.dumpElements().size() == ITEMS);
 }
 
 void BloomFilterSyncTest::testGetStrings(){
-    GenSync bfSync = GenSync::Builder().
+	GenSync bfSync = GenSync::Builder().
 			setProtocol(GenSync::SyncProtocol::BloomFilterSync).
 			setComm(GenSync::SyncComm::socket).
 			setBits(4).
 			setExpNumElems(numExpElem).
 			build();
-
-    CPPUNIT_ASSERT(!bfSync.getName().empty());
+	
+	CPPUNIT_ASSERT(!bfSync.getName().empty());
 }
 
 void BloomFilterSyncTest::testBloomFilterParamMismatch(){
-    const int BITS = sizeof(randZZ());
-
-    GenSync GenSyncServer = GenSync::Builder().
+	const int BITS = sizeof(randZZ());
+	
+	GenSync GenSyncServer = GenSync::Builder().
 			setProtocol(GenSync::SyncProtocol::BloomFilterSync).
 			setComm(GenSync::SyncComm::socket).
 			setBits(BITS).
 			//Different number of expectedElements to ensure that mismatches cause failure properly
 			setExpNumElems(numExpElem + 100).
 			build();
-
+	
 	GenSync GenSyncClient = GenSync::Builder().
 			setProtocol(GenSync::SyncProtocol::BloomFilterSync).
 			setComm(GenSync::SyncComm::socket).
 			setBits(BITS).
 			setExpNumElems(numExpElem).
 			build();
-
+	
 	//(oneWay = false, probSync = true, syncParamTest = true, Multiset = false, largeSync = false)
 	CPPUNIT_ASSERT(!(syncTest(GenSyncClient, GenSyncServer, false, true, true, false, false)));
 }
