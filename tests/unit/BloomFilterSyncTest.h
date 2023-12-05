2bd5d96edaebb98192587de4ee2865eaa8db3d25
Anish Sinha <108232600+Anish701@users.noreply.github.com>
2023-12-05 10:33:20 -0500

    Fixed formatting of BloomFilterSyncTest.h

--- tests/unit/BloomFilterSyncTest.h
+++ tests/unit/BloomFilterSyncTest.h
@@ -10,48 +10,48 @@
 #include <cppunit/extensions/HelperMacros.h>
 
 class BloomFilterSyncTest : public CPPUNIT_NS::TestFixture {
-    CPPUNIT_TEST_SUITE(BloomFilterSyncTest);
-
-        CPPUNIT_TEST(BloomFilterSyncSetReconcileTest);
+	CPPUNIT_TEST_SUITE(BloomFilterSyncTest);
+	
+	CPPUNIT_TEST(BloomFilterSyncSetReconcileTest);
 		CPPUNIT_TEST(BloomFilterSyncLargeSetReconcileTest);
 		CPPUNIT_TEST(testAddElem);
-        CPPUNIT_TEST(testGetStrings);
+	CPPUNIT_TEST(testGetStrings);
 		CPPUNIT_TEST(testBloomFilterParamMismatch);
-
-    CPPUNIT_TEST_SUITE_END();
+	
+	CPPUNIT_TEST_SUITE_END();
 public:
-    BloomFilterSyncTest();
-
-    ~BloomFilterSyncTest() override;
-    void setUp() override;
-    void tearDown() override;
-
+	BloomFilterSyncTest();
+	
+	~BloomFilterSyncTest() override;
+	void setUp() override;
+	void tearDown() override;
+	
 	/**
 	 * Test reconciliation: This test does have a very small probability of failure (If 0 elements are reconciled) as
 	 * BloomFilterSync is a probabilistic sync but with a sufficiently large numExpElems and the current seed this isn't an issue,
 	 * even for a large amount of tests
 	 */
-    void BloomFilterSyncSetReconcileTest();
-
+	void BloomFilterSyncSetReconcileTest();
+	
 	/**
 	 * Test reconciliation of large sets using BloomFilterSync
 	 */
 	void BloomFilterSyncLargeSetReconcileTest();
-
+	
 	/**
 	 * Test adding elements
 	 */
 	void testAddElem();
-
+	
 	/**
- 	* Test that printElem() and getName() return some nonempty string
- 	*/
-    void testGetStrings();
-
+	* Test that printElem() and getName() return some nonempty string
+	*/
+	void testGetStrings();
+	
 	/**
- 	* Test that IBLT Sync reports failure properly with incompatible sync parameters (Different number of expected elements)
- 	*/
-    void testBloomFilterParamMismatch();
+	* Test that IBLT Sync reports failure properly with incompatible sync parameters (Different number of expected elements)
+	*/
+	void testBloomFilterParamMismatch();
 };
 
 #endif //CPISYNCLIB_BloomFilterSYNCTEST_H
