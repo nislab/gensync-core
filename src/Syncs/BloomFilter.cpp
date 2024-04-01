3ba8de31592f5c14d65ecece70f39adbfafdbc57
Anish701 <anish2sinha@gmail.com>
2024-03-31 20:56:38 -0400

    changed BloomFilter hash_type name to bf_hash_t for clarity

--- src/Syncs/BloomFilter.cpp
+++ src/Syncs/BloomFilter.cpp
@@ -49,12 +49,12 @@ float BloomFilter::getFalsePosProb(size_t numExpElems)
     return pow(0.5, exp);
 }
 
-hash_type BloomFilter::_hash(const ZZ& value, long kk)
+bf_hash_t BloomFilter::_hash(const ZZ& value, long kk)
 {
 	hash<string> shash;
 
-    hash_type hash_val = shash(toStr(value));
-    hash_type hash_kk = shash(to_string(kk));
+    bf_hash_t hash_val = shash(toStr(value));
+    bf_hash_t hash_kk = shash(to_string(kk));
 
     return shash( to_string(hash_val) + to_string(hash_kk) );
 }
